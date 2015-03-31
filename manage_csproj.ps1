01b671f88b0808d1947ffa1b44fe16b0c4cd33e1
timvw <github@timvw.be>
2015-03-31 13:38:55 +0200

    Initial commit

---
+++ manage_csproj.ps1
@@ -0,0 +1,34 @@
+function GetHintPaths {
+	param(
+		[string] $CsProjFile = 'C:\src\trunk\ICTEAM.Solution\ICTEAM.Project1\ICTEAM.Project1.csproj'
+	)
+	
+    [xml] $Xml = Get-Content $CsProjFile
+    $NsMgr = New-Object System.Xml.XmlNamespaceManager($Xml.NameTable)
+    $NsMgr.AddNamespace("ns", "http://schemas.microsoft.com/developer/msbuild/2003")
+	$Xml.SelectNodes("//ns:HintPath", $NsMgr) |% { New-Object PSObject -Property @{CsProjFile="$CsProjFile";HintPath=$_.InnerText} }
+}
+
+function GetProjectReferences {
+	param (
+		[string] $CsProjFile = 'C:\src\trunk\ICTEAM.Solution\ICTEAM.Project1\ICTEAM.Project1.csproj'
+	)
+
+	[xml] $Xml = Get-Content $CsProjFile
+	$Ns = New-Object System.Xml.XmlNamespaceManager($Xml.NameTable)
+	$Ns.AddNamespace("ns", "http://schemas.microsoft.com/developer/msbuild/2003")
+
+	$Xml.SelectNodes("//ns:ProjectReference", $Ns) | 
+		ForEach-Object { $_.GetAttribute("Include") } | 
+		% { New-Object PSObject -Property @{CsProjFile="$CsProjFile";ReferencedProject="$_"} }
+}
+
+function GetExternalProjectReferences {
+	param (
+		[string] $CsProjFile = 'C:\src\trunk\ICTEAM.Solution\ICTEAM.Project1\ICTEAM.Project1.csproj',
+		[string] $ProjectReferencesFilter = '..\..\*'
+	)
+	
+	GetProjectReferences -CsProjFile "$CsProjFile" |
+	Where-Object { $_.ReferencedProject -like $ProjectReferencesFilter }
+}
