7ef6dc4766e842165381799a4a6fb60b0b85de5c
timvw <github@timvw.be>
2015-04-04 09:16:46 +0200

    Modified GetReferencedAssemblies to load assemblies in separate appdomain/process. This avoids blocking the dlls

--- manage_csproj.ps1
+++ manage_csproj.ps1
@@ -38,7 +38,14 @@ function GetReferencedAssemblies {
 		[string] $AssemblyFile = 'C:\src\trunk\ICTEAM.Solution\ICTEAM.Project1\bin\debug\ICTEAM.Project1.dll'
 	)
 	
-	$Assembly = [System.Reflection.Assembly]::LoadFrom($AssemblyFile)
-	$Assembly.GetReferencedAssemblies() |
-	% { New-Object PSObject -Property @{AssemblyFile="$AssemblyFile";ReferencedAssembly="$_"} }
+	$ScriptBlock = {
+		param (
+			[string] $AssemblyFile = 'C:\src\trunk\ICTEAM.Solution\ICTEAM.Project1\bin\debug\ICTEAM.Project1.dll'
+		)
+
+		$Assembly = [System.Reflection.Assembly]::LoadFrom("$AssemblyFile")
+		$Assembly.GetReferencedAssemblies() |% { New-Object PSObject -Property @{AssemblyFile="$AssemblyFile";ReferencedAssembly="$_"} }
+	}
+	
+	powershell -Command $ScriptBlock -args "$AssemblyFile"
 }
