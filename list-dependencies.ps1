ec83e9c6a7a9c1d91cf874f03a7a53468c263bfd
Steven Robijns <steven.robijns@prodatamobility.com>
2015-08-14 11:30:21 +0200

    Changed excel path to working path

--- list-dependencies.ps1
+++ list-dependencies.ps1
@@ -3,12 +3,12 @@
 # The line ". .\manage_csproj.ps1" will only work when you change your current working directory to the directory of the manage_csproj.ps1 file when executing from PowerShell ISE
 # To do that just open the script in the powershell_ise and type cd YOURDISK:\YOURWORKSPACE\powershell-dotnet-tools in the command line
 param( 
-    [string] $ExcelExePath = "C:\Program Files\Microsoft Office\Office14\EXCEL.EXE",
-	[string] $BasePath = 'C:\workspace\MobiguiderV2\trunk\MobiGuider.TariffManagement\Pms.TariffManagement.UI.Shell\bin\Debug\DirectoryModules\',
+    [string] $ExcelExePath = "C:\Program Files (x86)\Microsoft Office\Office14\EXCEL.EXE",
+	[string] $BasePath = 'C:\git\afc\AFC-Sales\Pms.Sales.Service.Agents\bin\Debug',
 	[string] $OutFile = 'C:\temp\' + $BasePath.Replace("\","_").Replace(":","").Replace(" ","").ToLowerInvariant() + '_' +(get-date -format "yyyy-MM-dd_HH-mm-ss") + '.csv'
 )
 
-. .\manage_csproj.ps1
+ .\manage_csproj.ps1
 
 #get the content for the csv file
 Get-ChildItem "$BasePath" -Name '*.dll' -Recurse |% { "$BasePath\$_" } |% { GetReferencedAssemblies -AssemblyFile "$_" } | Export-Csv "$OutFile"
